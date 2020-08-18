77ff6bd634fbefec9e6bd732bc3f4cf79ab59236
Pablo Diaz <diazpablo@gmail.com>
2020-08-18 22:55:18 +0200

    Table Component

---
+++ src/components/common/table.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+import PropTypes from "prop-types";
+
+const Table = ({ data, columns, sortColumn, onSort }) => {
+	return (
+		<table className="table table-hover">
+			<TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+			<TableBody data={data} columns={columns} />
+		</table>
+	);
+};
+
+
+Table.propTypes = {
+	data: PropTypes.array.isRequired,
+	columns: PropTypes.array.isRequired,
+	sortColumn: PropTypes.object.isRequired,
+	onSort: PropTypes.func.isRequired
+}
+
+
+export default Table;
